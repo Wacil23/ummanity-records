80334e6ba7ceb405a44a3fb6d78ed425efe3eb4c
Wacil23 <87616406+Wacil23@users.noreply.github.com>
2025-02-13 12:34:56 +0100

    remove secrets

--- src/services/shopify/mysadaka/Mysadaka.tsx
+++ src/services/shopify/mysadaka/Mysadaka.tsx
@@ -4,11 +4,11 @@ import { createAdminApiClient } from "@shopify/admin-api-client";
 export const mySadakaClient = createStorefrontApiClient({
   storeDomain: "https://mysadaka.fr",
   apiVersion: "2025-01",
-  publicAccessToken: "e0708cbad51d6ffd457eed2bae84fa6c",
+  publicAccessToken: process.env.MYSADAKA_PUBLIC_ACCESS_TOKEN ?? "",
 });
 
 export const mySadakaAdminClient = createAdminApiClient({
   storeDomain: "341f53-3.myshopify.com",
   apiVersion: "2024-10",
-  accessToken: "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",
+  accessToken: process.env.MYSADAKA_ACCESS_TOKEN ?? "",
 });
--- src/services/shopify/wakala/Wakala.tsx
+++ src/services/shopify/wakala/Wakala.tsx
@@ -4,11 +4,11 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 export const wakalaClient = createStorefrontApiClient({
   storeDomain: "https://mywakala.com",
   apiVersion: "2025-01",
-  publicAccessToken: "6653d4a9699c568b211ec425a91fae8e",
+  publicAccessToken: process.env.WAKALA_PUBLIC_ACCESS_TOKEN,
 });
 
 export const wakalaAdminClient = createAdminApiClient({
   storeDomain: "b77596-61.myshopify.com",
   apiVersion: "2025-01",
-  accessToken: "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",
+  accessToken: process.env.WAKALA_ACCESS_TOKEN ?? "",
 });
